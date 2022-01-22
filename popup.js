9a045028617c5e9ad899a70fe53346ce13db5691
Anders Svan <e7andy@gmail.com>
2022-01-22 15:27:19 +0100

    Commit

--- popup.js
+++ popup.js
@@ -21,4 +21,23 @@ function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
+
+  console.log("test click");
+  fetch('https://garminbadges.com/api/test.php?value=ext_hello!')
+    .then(
+      function(response) {
+        if(response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function(data) {
+          console.log(data);
+        });
+      }
+    )
+    .catch(function(err) {
+    console.log('Fetch error:', err);
+    });
 }
