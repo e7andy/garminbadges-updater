36efb2e57af83a002bc1794a1a56932e83a2a9e0
Anders Svan <e7andy@gmail.com>
2022-01-22 21:16:36 +0100

    POC done

--- popup.js
+++ popup.js
@@ -17,27 +17,35 @@ changeColor.addEventListener("click", async () => {
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-function setPageBackgroundColor() {
+async function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
 
-  console.log("test click");
-  fetch('https://garminbadges.com/api/test.php?value=ext_hello!')
-    .then(
-      function(response) {
-        if(response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' + response.status);
-          return;
-        }
 
-        // Examine the text in the response
-        response.json().then(function(data) {
-          console.log(data);
-        });
-      }
-    )
-    .catch(function(err) {
-    console.log('Fetch error:', err);
-    });
+  console.log("Call garmin.com with fetch");
+  const garminResponse = await fetch('https://connect.garmin.com/modern/proxy/badge-service/badge/earned', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'nk': 'NT'
+    },
+  });
+  const garminContent = await garminResponse.json();
+  console.log("Garmin",garminContent, garminResponse);
+
+
+  console.log("Call garminbadges.com");
+  const gbResponse = await fetch('https://garminbadges.com/api/test.php', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(garminContent)
+  });
+  const gbContent = await gbResponse.json();
+
+  console.log("GB",gbContent);
 }
