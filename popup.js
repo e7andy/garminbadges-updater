a76a60cd86c5474c3d2bdc846e46935473b20db4
Anders Svan <e7andy@gmail.com>
2022-01-24 18:37:03 +0100

    Design options page. Cleanup unused code.

--- popup.js
+++ popup.js
@@ -1,51 +1,60 @@
 // Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+let changeColor = document.getElementById("update_button");
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
+// When the button is clicked, inject updateData into current page
 changeColor.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: updateData,
   });
 });
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-async function setPageBackgroundColor() {
+async function updateData() {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
 
+  //Check that username and email is set in extension options, if not open options
+  if(true) {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL('options.html'));
+    }
+  } else {
 
-  console.log("Call garmin.com with fetch");
-  const garminResponse = await fetch('https://connect.garmin.com/modern/proxy/badge-service/badge/earned', {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'nk': 'NT'
-    },
-  });
-  const garminContent = await garminResponse.json();
-  console.log("Garmin",garminContent, garminResponse);
+    console.log("Call garmin.com with fetch");
+    const garminResponse = await fetch('https://connect.garmin.com/modern/proxy/badge-service/badge/earned', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'nk': 'NT'
+      },
+    });
+    const garminContent = await garminResponse.json();
+    console.log("Garmin",garminContent, garminResponse);
 
 
-  console.log("Call garminbadges.com");
-  const gbResponse = await fetch('https://garminbadges.com/api/test.php', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(garminContent)
-  });
-  const gbContent = await gbResponse.json();
+    console.log("Call garminbadges.com");
+    const gbResponse = await fetch('https://garminbadges.com/api/test.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(garminContent)
+    });
+    const gbContent = await gbResponse.json();
 
-  console.log("GB",gbContent);
+    console.log("GB",gbContent);
+  }
 }
